8a47f7b23eec6d61ecc1ad69bcc766d3868f3172
Albin Otterhäll <dev@a12l.xyz>
2022-02-20 13:19:09 +0100

    Wrap durden

--- overlay.nix
+++ overlay.nix
@@ -2,7 +2,13 @@ final: prev:
 
 {
   arcan = prev.callPackage ./pkgs/servers/arcan {};
+  arcan-wrapped = prev.callPackage ./wrapper.nix {
+    name = "arcan-wrapped";
+  };
+  
   xarcan = prev.callPackage ./pkgs/servers/x11/xarcan {};
+
   durden = prev.callPackage ./pkgs/desktops/durden {};
+
   pipeworld =prev.callPackage ./pkgs/applications/window-managers/pipeworld {};
 }
