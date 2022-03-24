bfed5b5765c44ca679dfe42911397e449b1ee9b1
Albin Otterhäll <dev@a12l.xyz>
2022-03-24 15:35:22 +0100

    Update Durden

--- pkgs/durden/default.nix
+++ pkgs/durden/default.nix
@@ -5,13 +5,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "durden";
-  version = "0.6.1+unstable=2022-01-30";
+  version = "0.6.1+unstable=2022-03-11";
 
   src = fetchFromGitHub {
     owner = "letoram";
     repo = pname;
-    rev = "117f4f8c0f694ea6fc9862bcfda5a652591b1106";
-    sha256 = "sha256-x8Ewx5gtRP9a0nd+TchoA0b9FfT1+W20Fbq0bf87Y2k=";
+    rev = "fec2a1051500df28becce188d932645743091efa";
+    sha256 = "sha256-uZ7oTnkWG3P/4ETgn6MQ9v47mRAJnvyzglQS7jlabPA=";
   };
 
   installPhase = ''
@@ -29,6 +29,5 @@ stdenv.mkDerivation rec {
     changelog = "https://github.com/letoram/durden/releases/tag/${version}";
     license = licenses.bsd3;
     platforms = platforms.all;
-    maintainers = with maintainers; [a12l];
   };
 }
